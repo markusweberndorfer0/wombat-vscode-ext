60daa1cd6048f39d664a816ac5aff1048545829e
Markus Weberndorfer <markus@weberndorfer.eu>
2025-04-05 14:22:59 +0200

    fix: console.log and comments

--- packages/extension/src/util.ts
+++ packages/extension/src/util.ts
@@ -4,12 +4,22 @@ import { API } from './api';
 import os from 'node:os';
 import { ProjectModel } from '../../shared/models/projectModel';
 
+/**
+ * Backups a project and re-downloads files, so files
+ * on the server wont be overridden
+ *
+ * If a project doesn't exist locally, it returns
+ * @param username
+ * @param projectname
+ * @param projectDir project directory
+ * @param project ProjectModel
+ */
 export function saveAndBackupProject(
     username: string,
     projectname: string,
     projectDir: string,
     project: ProjectModel
-) {
+): void {
     try {
         if (!fs.existsSync(projectDir)) {
             return;
